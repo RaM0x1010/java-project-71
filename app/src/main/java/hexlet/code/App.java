fc5925a011740866d02c88586988f76e6a34789d
Ruslan Mirzaliev w <ramirzaliev@gmail.com>
2026-03-24 10:01:01 +0300

    add checkstyle config and App modify prarms picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,8 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @Command(
@@ -14,14 +16,34 @@ import java.util.concurrent.Callable;
 
 public class App implements Callable<Integer> {
 
-    @Option(names = {"-f", "--format"},
-            description = "Output format (default: ${DEFAULT-VALUE})")
-    private String format = "stylish";
+    @Parameters(
+            index = "0",
+            description = "path to first file",
+            paramLabel = "FILE_0"
+    )
+    private File filepath1;
+
+    @Parameters(
+            index = "1",
+            description = "path to second file",
+            paramLabel = "FILE_1"
+    )
+    private File filepath2;
+
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: ${DEFAULT-VALUE}]",
+            defaultValue = "stylish"
+    )
+
+    private String format;
 
     @Override
     public Integer call() throws Exception {
-        // Your business logic for comparing files would go here
-        // For now, just print that the app is running
+        System.out.println("Comparing files:");
+        System.out.println("  File 1: " + filepath1.getAbsolutePath());
+        System.out.println("  File 2: " + filepath2.getAbsolutePath());
+        System.out.println("  Format: " + format);
         System.out.println("Running gendiff with format: " + format);
         return 0;
     }
